2ad114dbfe35f2526b8729878cc956cc1179a526
NotSusp <dumitrache.dan@proton.me>
2023-12-09 09:19:30 +0200

    Add project files.

---
+++ Models/Author.cs
@@ -0,0 +1,14 @@
+﻿namespace Dumitrache_Dan_Lab2.Models
+{
+    public class Author
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName
+        {
+            get { return string.Format("{0} {1}", FirstName, LastName); }
+        }
+        public ICollection<Book>? Books { get; set; }
+    }
+}
---
+++ Pages/Books/Edit.cshtml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Dumitrache_Dan_Lab2.Data;
+using Dumitrache_Dan_Lab2.Models;
+
+namespace Dumitrache_Dan_Lab2.Pages.Books
+{
+    public class EditModel : BookCategoriesPageModel
+    {
+        private readonly Dumitrache_Dan_Lab2.Data.Dumitrache_Dan_Lab2Context _context;
+
+        public EditModel(Dumitrache_Dan_Lab2.Data.Dumitrache_Dan_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Book = await _context.Book
+             .Include(b => b.Publisher)
+             .Include(b => b.BookCategories).ThenInclude(b => b.Category)
+             .AsNoTracking()
+             .FirstOrDefaultAsync(m => m.ID == id);
+
+
+            var book = await _context.Book.FirstOrDefaultAsync(m => m.ID == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            PopulateAssignedCategoryData(_context, Book);
+            Book = book;
+            
+            ViewData["AuthorID"] = new SelectList(_context.Set<Author>(), "ID",
+"FirstName", "LastName");
+
+            ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID",
+"PublisherName");
+            return Page();
+        }
+
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync(int? id, string[]
+selectedCategories)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            //se va include Author conform cu sarcina de la lab 2
+            var bookToUpdate = await _context.Book
+            .Include(i => i.Publisher)
+            .Include(i => i.BookCategories)
+            .ThenInclude(i => i.Category)
+            .FirstOrDefaultAsync(s => s.ID == id);
+            if (bookToUpdate == null)
+            {
+                return NotFound();
+            }
+            //se va modifica AuthorID conform cu sarcina de la lab 2
+            if (await TryUpdateModelAsync<Book>(
+            bookToUpdate,
+            "Book",
+            i => i.Title, i => i.Author,
+            i => i.Price, i => i.PublishingDate, i => i.PublisherID))
+            {
+                UpdateBookCategories(_context, selectedCategories, bookToUpdate);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
+            }
+            //Apelam UpdateBookCategories pentru a aplica informatiile din checkboxuri la entitatea Books care
+            //este editata
+            UpdateBookCategories(_context, selectedCategories, bookToUpdate);
+            PopulateAssignedCategoryData(_context, bookToUpdate);
+            return Page();
+        }
+    }
+}
