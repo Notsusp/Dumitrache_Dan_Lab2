c1bc1f5f71e98c99b255bdf466ef141cc85e1e34
NotSusp <dumitrache.dan@proton.me>
2023-12-09 09:19:30 +0200

    Add project files.

---
+++ Data/Dumitrache_Dan_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Dumitrache_Dan_Lab2.Models;
+
+namespace Dumitrache_Dan_Lab2.Data
+{
+    public class Dumitrache_Dan_Lab2Context : DbContext
+    {
+        public Dumitrache_Dan_Lab2Context (DbContextOptions<Dumitrache_Dan_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Dumitrache_Dan_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Dumitrache_Dan_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+        public DbSet<Dumitrache_Dan_Lab2.Models.Author> Author { get; set; } = default!;
+    }
+}
---
+++ Pages/Books/Create.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Dumitrache_Dan_Lab2.Data;
+using Dumitrache_Dan_Lab2.Models;
+
+namespace Dumitrache_Dan_Lab2.Pages.Books
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Dumitrache_Dan_Lab2.Data.Dumitrache_Dan_Lab2Context _context;
+
+        public CreateModel(Dumitrache_Dan_Lab2.Data.Dumitrache_Dan_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            ViewData["AuthorFullName"] = new SelectList(_context.Set<Author>(), "ID", "FullName");
+            ViewData["AuthorLastName"] = new SelectList(_context.Set<Author>(), "ID", "LastName");
+            ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID",
+"PublisherName");
+            return Page();
+        }
+
+        [BindProperty]
+        public Book Book { get; set; } = default!;
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Book.Add(Book);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
